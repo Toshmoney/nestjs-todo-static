af539b8d0e0f1a560d9c0eaed8ad40d00721e366
Toshmoney <abdulmaleeqomotosho@gmail.com>
2026-06-06 03:19:53 +0100

    todos controller

--- src/todos/todos.controller.ts
+++ src/todos/todos.controller.ts
@@ -1,6 +1,35 @@
 import { Controller } from '@nestjs/common';
+import { Body, Delete, Get, Param, Patch, Post, ParseIntPipe, ValidationPipe } from '@nestjs/common';
+import { TodosService } from './todos.service';
+import { CreateTodoDto } from './dto/create-todo.dto';
+import { UpdateTodoDto } from './dto/update-todo.dto';
 
 @Controller('todos')
 export class TodosController {
-    
+    constructor(private readonly todosService: TodosService) {}
+
+    @Post()
+    create(@Body(ValidationPipe) createTodoDto: CreateTodoDto) {
+        return this.todosService.create(createTodoDto);
+    }
+
+    @Get()
+    findAll() {
+        return this.todosService.findAll();
+    }
+
+    @Get(':id')
+    findOne(@Param('id', ParseIntPipe) id: number) {
+        return this.todosService.findOne(id);
+    }
+
+    @Patch(':id')
+    update(@Param('id', ParseIntPipe) id: number, @Body(ValidationPipe) updateTodoDto: UpdateTodoDto) {
+        return this.todosService.update(id, updateTodoDto);
+    }
+
+    @Delete(':id')
+    remove(@Param('id', ParseIntPipe) id: number) {
+        return this.todosService.remove(id);
+    }
 }
--- src/todos/todos.service.ts
+++ src/todos/todos.service.ts
@@ -1,4 +1,63 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException} from '@nestjs/common';
+import { CreateTodoDto } from './dto/create-todo.dto';
+import { UpdateTodoDto } from './dto/update-todo.dto';
 
 @Injectable()
-export class TodosService {}
+export class TodosService {
+    private todos: CreateTodoDto[] = [
+        {
+            id: 1,
+            title: 'Buy groceries',
+            description: 'Milk, Bread, Cheese',
+            isCompleted: false
+        },
+        {
+            id: 2,
+            title: 'Clean the house',
+            description: 'Living room, Kitchen',
+            isCompleted: false
+        },
+        {
+            id: 3,
+            title: 'Finish project',
+            description: 'Complete the NestJS project',
+            isCompleted: false
+        }
+    ];
+
+    create(createTodoDto: CreateTodoDto) {
+        const newTodo = {
+            id: this.todos.length + 1,
+            ...createTodoDto
+        };
+        this.todos.push(newTodo);
+        return newTodo;
+    }
+
+    findAll(isCompleted?: boolean) {
+        if (isCompleted !== undefined) {
+            return this.todos.filter(todo => todo.isCompleted === isCompleted);
+        }
+        return this.todos;
+    }
+
+    findOne(id: number) {
+        const todo = this.todos.find(todo => todo.id === id);
+        if (!todo) {
+            throw new NotFoundException(`Todo with ID ${id} not found`);
+        }
+        return todo;
+    }
+
+    update(id: number, updateTodoDto: UpdateTodoDto) {
+        const todo = this.findOne(id);
+        Object.assign(todo, updateTodoDto);
+        return todo;
+    }
+
+    remove(id: number) {
+        const todo = this.findOne(id);
+        this.todos.splice(this.todos.indexOf(todo), 1);
+        return todo;
+    }
+}
